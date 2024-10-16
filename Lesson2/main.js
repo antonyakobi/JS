db5278a2499c1e8e99a5e94e6db93d32e611af41
antonyakobi <antonyakobi@yandex.by>
2024-10-16 22:34:42 +0300

    switch + ternOper

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -1,41 +1,69 @@
-let userChoice // Выбор пользователя
-let computerChoice // Выбор компьютера
-let isWinner = false // Флаг для определения наличия победителя
- 
+let userChoice; // Выбор пользователя
+let computerChoice; // Выбор компьютера
+let isWinner = false; // Флаг для определения наличия победителя
+
 while (!isWinner) {
-  userChoice = prompt('Выбери камень, ножницы или бумага:')
-  userChoice = userChoice.toLowerCase()
- 
-  let randomNum = Math.floor(Math.random() * 3) // Генерация случайного числа от 0 до 2
-  if (randomNum === 0) {
-    computerChoice = 'камень'
-  } else if (randomNum === 1) {
-    computerChoice = 'ножницы'
-  } else {
-    computerChoice = 'бумага'
+  userChoice = prompt("Выбери камень, ножницы или бумага:");
+  userChoice = userChoice.toLowerCase();
+
+  let randomNum = Math.floor(Math.random() * 3); // Генерация случайного числа от 0 до 2
+  
+  //   if (randomNum === 0) {
+  //     computerChoice = 'камень'
+  //   } else if (randomNum === 1) {
+  //     computerChoice = 'ножницы'
+  //   } else {
+  //     computerChoice = 'бумага'
+  //   }
+  
+  switch (randomNum) {
+    case 0:
+      computerChoice = "камень";
+      break;
+    case 1:
+      computerChoice = "ножницы";
+      break;
+    case 2:
+      computerChoice = "бумага";
+      break;
   }
- 
+
   if (
-    userChoice === 'камень' ||
-    userChoice === 'ножницы' ||
-    userChoice === 'бумага'
+    userChoice === "камень" ||
+    userChoice === "ножницы" ||
+    userChoice === "бумага"
   ) {
-    alert('Компьютер выбрал: ' + computerChoice)
- 
+    alert("Компьютер выбрал: " + computerChoice);
+
+    // if (userChoice === computerChoice) {
+    //   alert("Ничья, играем ещё раз!");
+    // } else if (
+    //   (userChoice === "камень" && computerChoice === "ножницы") ||
+    //   (userChoice === "ножницы" && computerChoice === "бумага") ||
+    //   (userChoice === "бумага" && computerChoice === "камень")
+    // ) {
+    //   alert("Ты выиграл!");
+    //   isWinner = true;
+    // } else {
+    //   alert("Ты проиграл!");
+    //   isWinner = true;
+    // }
+
     if (userChoice === computerChoice) {
-      alert('Ничья, играем ещё раз!')
-    } else if (
-      (userChoice === 'камень' && computerChoice === 'ножницы') ||
-      (userChoice === 'ножницы' && computerChoice === 'бумага') ||
-      (userChoice === 'бумага' && computerChoice === 'камень')
-    ) {
-      alert('Ты выиграл!')
-      isWinner = true
+        alert('Ничья, играем ещё раз!')
     } else {
-      alert('Ты проиграл!')
-      isWinner = true
-    }
+       const isUserWinner =
+        (userChoice === 'камень' && computerChoice === 'ножницы') ||
+        (userChoice === 'ножницы' && computerChoice === 'бумага') ||
+        (userChoice === 'бумага' && computerChoice === 'камень')
+     
+        const message = isUserWinner ? 'Ты победил!' : 'Ты проиграл!'
+        alert(message)
+     
+        isWinner = true
+     }
+
   } else {
-    alert('Пожалуйста, введи корректный ход: камень, ножницы или бумага.')
+    alert("Пожалуйста, введи корректный ход: камень, ножницы или бумага.");
   }
 }
