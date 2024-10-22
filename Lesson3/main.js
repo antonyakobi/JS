9f47ede46e68eaf09b916ba19bd50a49cdbda8c1
antonyakobi <antonyakobi@yandex.by>
2024-10-22 23:38:40 +0300

    1

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -1,10 +1,10 @@
 // Объявляем глобальную переменную для хранения золота
 let gold = 100
- 
+
 // Попытка построить башню
 let buildingName = 'Tower'
 let costGold = 30
- 
+
 // проверяем, хватает ли нам золота для строительства
 if (gold >= costGold) {
     // вычитаем золото
@@ -12,4 +12,15 @@ if (gold >= costGold) {
     console.log(`${buildingName}: work complete!`);
 } else {
     console.log(`${buildingName}: not enough resources!`);
+}
+
+// Попытка построить кузницу
+buildingName = 'Blacksmith'
+costGold = 140
+ 
+if (gold >= costGold) {
+    gold -= costGold
+    console.log(`${buildingName}: work complete!`)
+} else {
+    console.log(`${buildingName}: not enough resources!`)
 }
